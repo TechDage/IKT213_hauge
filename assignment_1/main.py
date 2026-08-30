77b1115298d772613cbf256fb81b4b713a1dc0e4
TechDage <dehauge@uia.no>
2026-08-30 20:29:17 +0200

    Refactor main function and save camera information
    Refactor main function to always print image information and save camera data to a file.

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -4,7 +4,6 @@ import numpy as np
 #from keras.models import load_model
 #from keras.preprocessing.image import ImageDataGenerator
 #img = cv2.imread('iris-1.jpg',0)
-
 def print_image_information(image):
     height, width, channels = image.shape
     size = image.size
@@ -24,19 +23,15 @@ def save_camera_information():
     fps = cap.get(cv2.CAP_PROP_FPS)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-
     cap.release()
+
     with open("camera_outputs.txt", "w") as f:
         f.write(f"fps: {int(fps)}\n")
         f.write(f"height: {int(height)}\n")
         f.write(f"width: {int(width)}\n")
-
 def main():
     image = cv2.imread("iris-1.jpg")
-    if image is None:
-        print("Error")
-    else:
-        print_image_information(image)
+    print_image_information(image)
     save_camera_information()
 if __name__ == "__main__":
     main()
